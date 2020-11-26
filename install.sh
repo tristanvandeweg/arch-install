e0acda6168d39e49dfbef429aa890bd29f45d44c
tristanvandeweg <30982254+tristanvandeweg@users.noreply.github.com>
2020-11-26 18:12:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # install intel drivers base packages and xorg
-sudo pacman -S xf86-video-intel xorg xorg-xinit konsole firefox git base-devel vim
+sudo pacman -S xf86-video-intel xorg xorg-xinit konsole firefox git base-devel vim --noconfirm
 # install yay
 git clone https://aur.archlinux.org/yay-git.git
 cd ./yay-git/
 makepkg -si
 cd ./..
 # install kde
-sudo pacman -S plasma lightdm lightdm-gtk-greeter
+sudo pacman -S plasma sddm blender gimp libreoffice --noconfirm
